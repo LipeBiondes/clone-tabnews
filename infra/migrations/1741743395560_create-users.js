303525d84acce06791126dbdfc10ff8d7c20860a
Alefe Filipe Biondes Magalhães <alefefelipebiondesmagalhaes@gmail.com>
2025-03-20 19:06:26 -0300

    feat: add migration to create `users` table

--- infra/migrations/1741743395560_create-users.js
+++ infra/migrations/1741743395560_create-users.js
@@ -11,10 +11,27 @@ exports.up = (pgm) => {
       notNull: true,
       unique: true,
     },
+    // Why 254? Because it's the maximum length of an email address. See https://stackoverflow.com/a/574698/376773.
     email: {
       type: "varchar(254)",
       notNull: true,
       unique: true,
+    },
+    // Why 60? Because it's the maximum length of a bcrypt hash. See https://www.npmjs.com/package/bcrypt#hash-info.
+    password: {
+      type: "varchar(60)",
+      notNull: true,
+    },
+    // Why timestamptz with timezone? Because it stores the date and time in UTC format, which is the best practice for storing dates and times in databases.
+    created_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('UTC', now())"),
+    },
+    updated_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('UTC', now())"),
     },
   });
 };
