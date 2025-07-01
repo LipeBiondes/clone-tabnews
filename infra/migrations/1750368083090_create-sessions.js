0e3cca111246507ff1e7876ef420c28839dceea0
Lipe biondes <alefefelipebiondesmagalhaes@gmail.com>
2025-07-01 19:59:47 -0300

    feat: feat: implement session creation
    implement session creation

---
+++ infra/migrations/1750368083090_create-sessions.js
@@ -0,0 +1,35 @@
+exports.up = (pgm) => {
+  pgm.createTable("sessions", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+    token: {
+      type: "varchar(96)",
+      notNull: true,
+      unique: true,
+    },
+    user_id: {
+      type: "uuid",
+      notNull: true,
+    },
+    // Why timestamptz with timezone? Because it stores the date and time in UTC format, which is the best practice for storing dates and times in databases.
+    expires_at: {
+      type: "timestamptz",
+      notNull: true,
+    },
+    created_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('UTC', now())"),
+    },
+    updated_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('UTC', now())"),
+    },
+  });
+};
+
+exports.down = false;
